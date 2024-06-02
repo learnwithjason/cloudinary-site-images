acbb1f881c32ecd30d51787be04bdf79ccca1e5c
Jason Lengstorf <jason@lengstorf.com>
2024-06-02 13:03:22 -0700

    feat: updates for deployed DB

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { imageService } from '@unpic/astro/service';
 import { defineConfig } from 'astro/config';
-
-import db from "@astrojs/db";
+import db from '@astrojs/db';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  image: {
-    service: imageService()
-  },
-  integrations: [db()]
+	output: 'hybrid',
+	image: {
+		service: imageService(),
+	},
+	integrations: [db()],
+	adapter: netlify(),
 });
